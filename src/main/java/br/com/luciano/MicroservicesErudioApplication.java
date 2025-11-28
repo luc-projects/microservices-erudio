5ee5bf1e2ef75d66771c1f991b90646afb2676c7
luc-projects <luc.projects@hotmail.com>
2025-11-28 17:39:38 -0300

    initial commit

---
+++ src/main/java/br/com/luciano/MicroservicesErudioApplication.java
@@ -0,0 +1,13 @@
+package br.com.luciano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesErudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesErudioApplication.class, args);
+	}
+
+}
